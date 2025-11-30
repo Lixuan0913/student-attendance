cd9f96746c00e261fc7bcbade44bb333af601d33
Li Xuan <lixuanyap4@gmail.com>
2025-11-30 15:29:52 +0800

    add file

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// *********************************************************
+// Program: YOUR_FILENAME.cpp
+// Course: CCP6114 Programming Fundamentals
+// Lecture Class: TC1L
+// Tutorial Class: TT4L
+// Trimester: 2530
+// Member_1: ID | NAME | EMAIL | PHONE
+// Member_2: ID | NAME | EMAIL | PHONE
+// Member_3: ID | NAME | EMAIL | PHONE
+// Member_4: ID | NAME | EMAIL | PHONE
+// *********************************************************
+// Task Distribution
+// Member_1:
+// Member_2:
+// Member_3:
+// Member_4:
+// *********************************************************
+
+
+#include <iosteam>
+
+int main(){
+    cout << "Hello"
+}
