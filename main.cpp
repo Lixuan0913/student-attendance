56e68089e24a0f0242d6771fe242ff8eaef5096e
Li Xuan <lixuanyap4@gmail.com>
2025-12-06 14:58:45 +0800

    Add header and input variable

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 // *********************************************************
-// Program: YOUR_FILENAME.cpp
+// Program: TC1L_Group01_main.cpp
 // Course: CCP6114 Programming Fundamentals
 // Lecture Class: TC1L
-// Tutorial Class: TT4L
+// Tutorial Class: TT1L
 // Trimester: 2530
-// Member_1: ID | NAME | EMAIL | PHONE
-// Member_2: ID | NAME | EMAIL | PHONE
-// Member_3: ID | NAME | EMAIL | PHONE
-// Member_4: ID | NAME | EMAIL | PHONE
+// Member_1: 252UC243XL | AISYAH BINTI AHMAD HAWARI | AISYAH.AHMAD.HAWARI1@student.mmu.edu.my | 01110691624
+// Member_2: 252UC24216 | AMIRA SOFIA BINTI AZIZUL RAHMA | AMIRA.SOFIA.AZIZUL1@student.mmu.edu.my | 01156786588
+// Member_3: 252UC241RN | ELLY MAZLIN BINTI MOHD AZMIR | ELLY MAZLIN BINTI MOHD AZMIR | 012662 3767
+// Member_4: 252UC242JW | YAP LI XUAN | yap.li.xuan1@student.mmu.edu.my | 0126371496
 // *********************************************************
 // Task Distribution
 // Member_1:
@@ -16,9 +16,12 @@
 // Member_4:
 // *********************************************************
 
-
-#include <iosteam>
+#include <fstream>
+#include <iostream>
+using namespace std;
 
 int main(){
-    cout << "Hello";
+    string sheet_name;
+    cout << "Enter attendance sheet name: ";
+    cin >> sheet_name;
 }
