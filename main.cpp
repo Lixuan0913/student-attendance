9948b2cde9cb820c7e27ebe962707885a7d94daf
Amira Sofia <amirasazizul@yahoo.com>
2025-12-14 16:17:05 +0800

    Creating sheet

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // Tutorial Class: TT4L
 // Trimester: 2530
 // Member_1: ID | NAME | EMAIL | PHONE
-// Member_2: ID | NAME | EMAIL | PHONE
+// Member_2: 252UC24216 | Amira Sofia Azizul | amira.sofia.azizul1@student.mmu.edu.my | 01156786588
 // Member_3: ID | NAME | EMAIL | PHONE
 // Member_4: ID | NAME | EMAIL | PHONE
 // *********************************************************
@@ -17,8 +17,42 @@
 // *********************************************************
 
 
-#include <iosteam>
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main(){
-    cout << "Hello";
+int main() {
+    string sheetName;
+    int numColumns;
+    string columnNames[10];
+
+    cout << " ===========================================" << endl;
+    cout << "  STUDENT ATTENDANCE TRACKER - MILESTONE 1 " << endl;
+    cout << " ===========================================" << endl << endl;
+
+    cout << "Enter attendance sheet name: ";
+    getline(cin, sheetName);
+    cout << "Attendance sheet \"" << sheetName << "\" created successfully.\n\n";
+
+    // create number of columns
+    do {
+        cout << "Define number of columns (max 10): ";
+        cin >> numColumns;
+
+        if (numColumns < 1 || numColumns > 10) {
+            cout << "Please enter a number between 1 and 10.\n";
+        }
+    } while (numColumns < 1 || numColumns > 10);
+
+    cin.ignore(); // clear unwanted character
+
+    //create column names
+    for (int i = 0; i < numColumns; i++) {
+        cout << "Enter column " << (i + 1) << " name: ";
+        getline(cin, columnNames[i]);
+    }
+
+    cout << "\nSheet structure created successfully.\n";
+
+    return 0;
 }
