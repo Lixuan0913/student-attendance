cc150ebfe2194b367b305f4cd0463a30ce844a1b
Li Xuan <lixuanyap4@gmail.com>
2025-12-25 20:52:13 +0800

    Add insert function

--- main.cpp
+++ main.cpp
@@ -19,9 +19,12 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <limits>
 using namespace std;
 
 void content(string);
+void insertData(string);
 
 int main() {
     ifstream inputFile;
@@ -29,6 +32,7 @@ int main() {
     string sheetName;
     int numColumns;
     string columnNames[10];
+    char choice;
 
     cout << " ===========================================" << endl;
     cout << "  STUDENT ATTENDANCE TRACKER - MILESTONE 1 " << endl;
@@ -61,13 +65,12 @@ int main() {
     }
 
     //input in text file
-    outputFile << "Attendance Sheet: " << sheetName << "\n\n";
 
 
     for (int i = 0; i < numColumns; i++) {
         outputFile << columnNames[i];
         if (i < numColumns - 1) {
-            outputFile << "\t";
+            outputFile << ",";
         }
     }
     outputFile << "\n";
@@ -76,6 +79,110 @@ int main() {
 
     cout << "\nSheet structure created successfully and saved to '" << sheetName << ".txt'.\n";
 
+    do{
+    cout << "Do you want insert data?(y/n):\n";
+    cin >> choice;
+
+      if(choice == 'y' || choice == 'Y')
+         insertData(sheetName);
+      else if(choice == 'n' || choice == 'N'){
+        cout << "Goodbye!\n";
+        break;
+      }
+
+      else
+         cout << "Please enter either y or n\n";
+
+    }while(true);
+
+
     return 0;
+}
+
+void insertData(string sheetname){
+   vector<string> columns;
+   string line;
+
+   string filename = sheetname + ".txt";
+   ifstream inputFile(filename);
+
+   if (!inputFile) {
+        cout << "Error opening file!" << endl;
+
+    }
+
+    getline(inputFile, line);
+    inputFile.close();
+
+    //Put all header to an vector
+    while(!line.empty()){
+        // Seperate the value by checking comma
+        int commaFound = line.find(",");
+        //
+        string col = (commaFound!=1)? line.substr(0,commaFound) : line;
+
+        while (!col.empty() && col[0] == ' ') col.erase(0, 1);
+        while (!col.empty() && col[col.size()-1] == ' ') col.pop_back();
+
+        columns.push_back(col);
+
+        line = (commaFound != -1) ? line.substr(commaFound + 1) : "";
+
+    }
+
+
+   cout << "\n-------------------------------------------\n";
+   cout << "Insert New Attendance Row\n";
+   cout << "-------------------------------------------\n";
+
+   int size = columns.size();
+   string userInput[size];
+
+   ofstream outputfile(filename, ios::app);
+
+   if(!outputfile) cout << "Error opening file for writing!\n";
+
+   cin.ignore();
+
+
+   for (int i = 0; i < size; i++) {
+       while(true){
+
+            if(columns[i] == "STATUS" || columns[i] == "status" || columns[i] == "Status")
+                cout << "Enter " << columns[i] << " (Present: 1, Absent: 0): ";
+
+            else
+              cout << "Enter " << columns[i] << ":";
+
+            getline(cin, userInput[i]);
+
+            // Check if StudentID are digits
+            if(i == 0){
+               bool valid = true;
+               for(char c : userInput[i]){
+                    if(!isdigit(c))
+                       valid = false;
+               }
+
+            if(!valid){
+                cout << "Invalid INT value. Please enter a number." << endl;
+                continue;
+            }
+
+            }
+
+        break;
+    }
+    //Insert value into file
+    outputfile << userInput[i];
+    if(i != size - 1) outputfile << ",";
+
+   }
+   outputfile << "\n";
+   outputfile.close();
+
+ cout << "\nRow inserted successfully.\n";
+    /*for(int i = 0; i < size; i++)
+        cout << columns[i] << ": " << userInput[i] << endl;* for debug use/
 }
 
